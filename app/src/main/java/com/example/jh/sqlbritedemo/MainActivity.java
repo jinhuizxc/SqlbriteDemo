8bd546847d6b60f727d52e7e312541d8b286acba
JinHui <1004260403@qq.com>
2017-10-18 09:27:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/sqlbritedemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.jh.sqlbritedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * SqlBriteDemo
+ *
+ * com.squareup.sqlbrite.BriteDatabase 的学习！项目需要
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
